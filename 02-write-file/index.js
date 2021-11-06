721f328ace6f8f99653d825cb3ef72da64149d99
Yauheny-Bychkou <55757666+Yauheny-Bychkou@users.noreply.github.com>
2021-11-06 19:05:45 +0300

    02-write-file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,23 +1,31 @@
-// const fs = require("fs");
-// const path = require("path");
-// const filePath = path.join(__dirname, "text.txt");
-// fs.writeFile(filePath, "", (err) => {
-//   if (err) {
-//     throw err;
-//   }
-// });
-// const { stdin, stdout } = process;
-// stdout.write("Введите текст ");
-// stdin.on("data", (data) => {
-//   const dataStringified = data.toString();
-//   if (dataStringified.indexOf("exit") === -1) {
-//     fs.appendFile(filePath, data, (err) => {
-//       if (err) {
-//         throw err;
-//       }
-//     });
-//   } else {
-//     stdout.write("Прощай...");
-//     process.exit();
-//   }
-// });
+const fs = require("fs");
+const path = require("path");
+const readline = require("readline");
+const { stdin: input, stdout: output } = require("process");
+const filePath = path.join(__dirname, "text.txt");
+const rl = readline.createInterface({ input, output });
+const { stdin, stdout } = process;
+
+fs.writeFile(filePath, "", (err) => {
+  if (err) {
+    throw err;
+  }
+});
+stdout.write(`Hello! Enter the text: `);
+
+rl.on("line", (input) => {
+  if (input.indexOf("exit") === -1) {
+    fs.appendFile(filePath, `\n${input}`, (err) => {
+      if (err) {
+        throw err;
+      }
+    });
+  } else if (input === "exit") {
+    stdout.write("Goodbye!");
+    process.exit(0);
+  }
+});
+rl.on("close", () => {
+  stdout.write("Goodbye!");
+  process.exit(0);
+});
