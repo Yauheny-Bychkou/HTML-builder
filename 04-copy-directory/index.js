e83df2e7490eeca1d7a73b324cfa72860aa42b5f
Yauheny-Bychkou <55757666+Yauheny-Bychkou@users.noreply.github.com>
2021-11-06 19:11:12 +0300

    04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,23 +1,23 @@
-// const fs = require("fs");
-// const path = require("path");
-// const fsPromises = fs.promises;
-// const fileFiles = path.join(__dirname, "files");
-// const fileCopy = path.join(__dirname, "files-copy");
+const fs = require("fs");
+const path = require("path");
+const fsPromises = fs.promises;
+const fileFiles = path.join(__dirname, "files");
+const fileCopy = path.join(__dirname, "files-copy");
 
-// const copyFolder = async () => {
-//   await fsPromises.mkdir(fileCopy, { recursive: true });
-// };
-// const copyFiles = async () => {
-//   await fs.readdir(fileFiles, (err, items) => {
-//     items.forEach((item) => {
-//       fs.copyFile(path.join(fileFiles, item), path.join(fileCopy, item), (err) => {
-//         if (err) {
-//           throw err;
-//         }
-//       });
-//     });
-//   });
-// };
+const copyFolder = async () => {
+  await fsPromises.mkdir(fileCopy, { recursive: true });
+};
+const copyFiles = async () => {
+  await fs.readdir(fileFiles, (err, items) => {
+    items.forEach((item) => {
+      fs.copyFile(path.join(fileFiles, item), path.join(fileCopy, item), (err) => {
+        if (err) {
+          throw err;
+        }
+      });
+    });
+  });
+};
 
-// copyFolder();
-// copyFiles();
+copyFolder();
+copyFiles();
