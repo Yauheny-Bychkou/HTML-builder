c85aa49af7a76a98350807b72ff1751104e78d54
Yauheny-Bychkou <55757666+Yauheny-Bychkou@users.noreply.github.com>
2021-11-06 19:12:39 +0300

    feat: 05-merge-styles

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,23 +1,23 @@
-// const fs = require("fs");
-// const path = require("path");
-// const filePath = path.join(__dirname, "project-dist", "bundle.css");
-// const fileStyle = path.join(__dirname, "styles");
+const fs = require("fs");
+const path = require("path");
+const filePath = path.join(__dirname, "project-dist", "bundle.css");
+const fileStyle = path.join(__dirname, "styles");
 
-// fs.readdir(fileStyle, (err, files) => {
-//   if (err) {
-//     throw err;
-//   }
-//   files.forEach((item) => {
-//     if (item.split(".")[1] === "css") {
-//       const readableStream = fs.createReadStream(path.join(fileStyle, item));
+fs.readdir(fileStyle, (err, files) => {
+  if (err) {
+    throw err;
+  }
+  files.forEach((item) => {
+    if (item.split(".")[1] === "css") {
+      const readableStream = fs.createReadStream(path.join(fileStyle, item));
 
-//       readableStream.on("data", (data) => {
-//         fs.appendFile(filePath, data, (err) => {
-//           if (err) {
-//             throw err;
-//           }
-//         });
-//       });
-//     }
-//   });
-// });
+      readableStream.on("data", (data) => {
+        fs.appendFile(filePath, data, (err) => {
+          if (err) {
+            throw err;
+          }
+        });
+      });
+    }
+  });
+});
