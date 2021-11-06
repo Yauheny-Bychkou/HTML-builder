c1b04731789f46d9a064d2dd14ac87539b30ab1a
Yauheny-Bychkou <55757666+Yauheny-Bychkou@users.noreply.github.com>
2021-11-06 19:09:46 +0300

    feat: 03-files-in-folder

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,25 +1,25 @@
-// const fs = require("fs");
-// const path = require("path");
-// const filePath = path.join(__dirname, "secret-folder");
-// const arr = [];
+const fs = require("fs");
+const path = require("path");
+const filePath = path.join(__dirname, "secret-folder");
+const arr = [];
 
-// fs.readdir(filePath, { withFileTypes: true }, (err, files) => {
-//   if (err) {
-//     throw err;
-//   }
-//   files.forEach((item) => {
-//     if (item.isFile()) {
-//       arr.push(item);
-//     }
-//   });
-//   arr.forEach((elem) => {
-//     const pathh = path.join(__dirname, "secret-folder", elem.name);
-//     fs.stat(pathh, (err, stats) => {
-//       if (err) {
-//         throw err;
-//       }
+fs.readdir(filePath, { withFileTypes: true }, (err, files) => {
+  if (err) {
+    throw err;
+  }
+  files.forEach((item) => {
+    if (item.isFile()) {
+      arr.push(item);
+    }
+  });
+  arr.forEach((elem) => {
+    const pathh = path.join(__dirname, "secret-folder", elem.name);
+    fs.stat(pathh, (err, stats) => {
+      if (err) {
+        throw err;
+      }
 
-//       console.log(`${elem.name.split(".")[0]} - ${path.extname(elem.name).split(".")[1]} - ${stats.size / 1024}kb`);
-//     });
-//   });
-// });
+      console.log(`${elem.name.split(".")[0]} - ${path.extname(elem.name).split(".")[1]} - ${stats.size / 1024}kb`);
+    });
+  });
+});
